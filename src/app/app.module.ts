b233bbe40843dcca54ee4e91f2500c01aa87a913
Alejandro Dueñas <luisdue3@gmail.com>
2024-04-29 21:14:27 -0500

    Se agrega el modulo routing-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app.routing-module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +9,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
+  exports: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
