fde906bc2dea8ffc07e64594a2c776704fd7a729
Alejandro Dueñas <luisdue3@gmail.com>
2024-05-20 17:17:51 -0500

    Creación componente padre

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'curso-angular';
+  title = 'Pedro';
+  subtitle = 'Es la persona que esta logeada';
 }
