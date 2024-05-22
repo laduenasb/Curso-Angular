5390193612d95016a5e6e1046cba57519dcc432a
Alejandro Dueñas <luisdue3@gmail.com>
2024-05-21 20:26:45 -0500

    Creación componente hijo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app.routing-module';
 import { AppComponent } from './app.component';
 import { PadreComponent } from './padre/padre.component';
+import { HijoComponent } from './padre/hijo/hijo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PadreComponent
+    PadreComponent,
+    HijoComponent
   ],
   imports: [
     BrowserModule,
